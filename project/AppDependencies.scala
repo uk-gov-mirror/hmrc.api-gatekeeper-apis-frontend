c9359c8e609c4f4d14b9ad12b33871b5656abf65
peteslater-ee <28756214+peteslater-ee@users.noreply.github.com>
2026-07-13 14:43:29 +0100

    API-9307 - Updating dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,15 +4,15 @@ object AppDependencies {
 
   private val bootstrapVersion    = "10.7.0"
 
-  private val apiDomainVersion    = "1.4.0"
-  private val appDomainVersion    = "1.1.0"
+  private val apiDomainVersion    = "1.5.0"
+  private val appDomainVersion    = "1.3.0"
   private val playfrontendVersion = "12.32.0"
 
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"      % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"      % playfrontendVersion,
-    "uk.gov.hmrc"       %% "internal-auth-client-play-30"    % "4.3.0",
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30"    % "4.4.0",
     "org.apache.commons" % "commons-csv"                     % "1.14.1",
     "uk.gov.hmrc"       %% "api-platform-api-domain"         % apiDomainVersion,
     "uk.gov.hmrc"       %% "api-platform-application-domain" % appDomainVersion
